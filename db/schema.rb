2e42d41968d75f071db395a28bc71e2e84781b3e
minooor <minorilax13@gmail.com>
2022-05-01 12:37:49 +0000

    Add ログイン/ログアウト機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_01_104232) do
+ActiveRecord::Schema.define(version: 2022_05_01_122245) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
