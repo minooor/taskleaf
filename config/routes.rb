ef63fbe17721af9762873a0ef436fcc5daf124f2
minooor <minorilax13@gmail.com>
2022-05-01 12:48:47 +0000

    Add 管理機能の制限

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/lodout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
 
   namespace :admin do
     resources :users
